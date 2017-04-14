a6d5ee619275f2d6ec9d714593c199f871699ed4
Ankush Goyal <goyalzz@users.noreply.github.com>
2017-04-14 11:28:48 +0530

    Update sts.sh

--- sts.sh
+++ sts.sh
@@ -8,7 +8,7 @@ CMD="docker run \
        --user 1000:1000 \
        --volume /tmp/.X11-unix:/tmp/.X11-unix \
        --volume $HOME:/home/developer \
-       kurron/docker-sts:latest"
+       goyalzz/docker-sts:latest"
 
 echo $CMD
 $CMD
